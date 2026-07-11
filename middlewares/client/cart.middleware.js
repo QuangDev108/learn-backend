66884ab7dffce8736e32c3ba5cd4448f116d671c
=QuangDev <=quangtruong0420@gmail.com>
2026-07-11 21:13:44 +0700

    Lam trang dat hang thanh cong

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,8 +1,18 @@
 const Cart = require("../../models/cart.model");
 
 module.exports.cartId = async (req, res, next) => {
-    if (!req.cookies.cartId) {
-        const cart = new Cart();
+    let cart = null;
+
+    if (req.cookies.cartId) {
+        cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        });
+    }
+
+    // Nếu không có cookie, hoặc cookie có nhưng cart đã bị xóa khỏi DB
+    // thì tạo cart mới để tránh lỗi "Cannot read properties of null"
+    if (!cart) {
+        cart = new Cart();
         await cart.save();
 
         const expiresTime = 1000 * 60 * 60 * 24 * 365;
@@ -10,18 +20,14 @@ module.exports.cartId = async (req, res, next) => {
         res.cookie("cartId", cart.id, {
             expires: new Date(Date.now() + expiresTime)
         });
-    } else {
-        const cart = await Cart.findOne({
-            _id: req.cookies.cartId
-        });
-
-        cart.totalQuantity = cart.products.reduce(
-            (sum, item) => sum + item.quantity,
-            0
-        );
-
-        res.locals.miniCart = cart;
     }
+
+    cart.totalQuantity = cart.products.reduce(
+        (sum, item) => sum + item.quantity,
+        0
+    );
+
+    res.locals.miniCart = cart;
 
     next();
 };
