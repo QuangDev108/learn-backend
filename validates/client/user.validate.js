737c8c678b90fc62b98f6ecde681364a4e974193
=QuangDev <=quangtruong0420@gmail.com>
2026-07-12 14:48:34 +0700

    Đăng nhập

--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -23,3 +23,22 @@ module.exports.registerPost = (req,res,next) => {
     next();
 
 }
+
+module.exports.loginPost = (req,res,next) => {
+    if(!req.body.email) {
+        console.log(req.body);
+        req.flash("error", `email ko dc để trống `);
+        res.redirect(req.get("Referrer") || "/");
+        return;
+    }
+
+    if(!req.body.password) {
+        console.log(req.body);
+        req.flash("error", `Mật khẩu ko dc để trống `);
+        res.redirect(req.get("Referrer") || "/");
+        return;
+    }
+
+    next();
+
+}
