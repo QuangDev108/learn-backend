ff2103c35e3b5317d34aec2f5b41825f19adca80
=QuangDev <=quangtruong0420@gmail.com>
2026-07-17 16:31:30 +0700

    Làm trang 404

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ app.use(express.static(`${__dirname}/public`));
 //Routes
 routeAdmin(app);
 routes(app);
+app.use((req, res) => {
+    res.status(404).render("client/pages/errors/404", {
+        pageTitle: "404 Not Found",
+    });
+});
 
 app.listen(port, () => {  
   console.log(`Server đang chạy tại http://localhost:${port}`);
