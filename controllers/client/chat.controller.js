ad2033c759eb93327e6edcea47f825199c375d7e
=QuangDev <=quangtruong0420@gmail.com>
2026-07-21 23:36:25 +0700

    Hiện thị hệ thống chat data realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,12 +1,47 @@
-// [GET] /chat
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
+
+// [GET] /chat/
 module.exports.index = async (req, res) => {
-    //Socket IO
-    _io.on("connection", (socket) => {
-        console.log("A user connected:", socket.id);
+    const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
+
+    // Socket.IO
+    _io.once("connection", (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+
+            // Lưu vào database
+            const chat = new Chat({
+                user_id: userId,
+                content: content,
+            });
+            await chat.save();
+
+            // Trả data về client
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: content,
+            });
+        });
     });
-    //End Socket IO
+    // End Socket.IO
+
+    // Lấy ra data
+    const chats = await Chat.find({
+        deleted: false,
+    });
+
+    for (const chat of chats) {
+        const infoUser = await User.findOne({
+            _id: chat.user_id,
+        }).select("fullName");
+
+        chat.infoUser = infoUser;
+    }
 
     res.render("client/pages/chat/index", {
         pageTitle: "Chat",
+        chats: chats,
     });
 };
