3485135e8ec48ba745d89740cb8a1d823f56f698
=QuangDev <=quangtruong0420@gmail.com>
2026-09-09 20:55:01 +0700

    Chức năng kết bạn

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,11 +1,30 @@
 const User = require("../../models/user.model");
+const usersSocket = require("../../sockets/clients/users.socket");
 
 // [GET] /users/not-friend
 module.exports.notFriend = async (req, res) => {
+    //Socket
+    usersSocket(res);
+    //End Socket
+
     const userId = res.locals.user.id;
 
+    const myUser = await User.findOne({
+        _id: userId
+    });
+
+    const requestFriends = myUser.requestFriends;
+    const acceptFriends = myUser.acceptFriends;
+
+    console.log(requestFriends);
+
     const users = await User.find({
-        _id: { $ne: userId },
+        $and: [
+            {_id: { $ne: userId } },
+            {_id: { $nin: requestFriends }},
+            {_id: { $nin: acceptFriends }},
+        ],
+        
         status: "active",
         deleted: false
     }).select("avatar fullName");
--- models/user.model.js
+++ models/user.model.js
@@ -6,21 +6,33 @@ const userSchema = new mongoose.Schema(
         fullName: String,
         email: String,
         password: String,
+
         tokenUser: {
             type: String,
             default: generate.generateRandomString(30)
         },
+
         phone: String,
         avatar: String,
+        friendList: [
+            {
+                user_id: String,
+            }
+        ],
+        acceptFriends: Array,
+        requestFriends: Array,
         status: {
             type: String,
             default: "active"
         },
+
         deleted: {
             type: Boolean,
             default: false
         },
+
         deletedAt: Date,
+
     },
     {
         timestamps: true
