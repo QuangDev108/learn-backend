107e9624d6d252be1ff33c671c7dd70072691863
=QuangDev <=quangtruong0420@gmail.com>
2026-09-09 23:10:12 +0700

    chức năng chấp nhận kết bạn

--- models/user.model.js
+++ models/user.model.js
@@ -17,6 +17,7 @@ const userSchema = new mongoose.Schema(
         friendList: [
             {
                 user_id: String,
+                room_chat_id: String
             }
         ],
         acceptFriends: Array,
